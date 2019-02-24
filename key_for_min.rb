c5c3f601093ff74d5dd115386403e4611326677b
Kate F <follett.katie@gmail.com>
2019-02-24 19:24:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,10 @@
 
 require "pry"
 def key_for_min_value(name_hash)
-  
+  # smallest_name_and_number is nil or a 2 element array
+  # like this: [name, number]
+  # it's a pair.
+  smallest_name_and_number = nil
   name_hash.each do |name, number|
-    if number == 1
-      return name
-    elsif number == 10
-      return name
-    end
   end
 end
